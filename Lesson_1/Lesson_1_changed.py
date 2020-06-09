371db0f282e5fb5738eeef48c37f83558dc3730d
Quicksilver-jay <quicksilver.jay@gmail.com>
2020-06-09 15:53:00 +0300

    frirs commit

---
+++ Lesson_1/Lesson_1_changed.py
@@ -0,0 +1,30 @@
+# Task_1
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print ("a + b = ",a+b)
+
+print ("a - b = ",a-b)
+
+print ("a * b = ",a*b)
+
+print ("a / b = ",a/b)
+
+# Task_2
+
+name = input ("What is your name?: ")
+age = input ("How old are you?: ")
+city = input ("Where do you live?: ")
+
+print ("Hello, ",name, "Your age is ",age, "You live in ",city)
+
+  
+print ("        ******         ")
+print ("     *         *      ")
+print ("    *   O    O   *     ")
+print ("   *              *   ")
+print ("   *   #       #  *    ")
+print ("    *    # # #   *   ")
+print ("      *        *     ")
+print ("        ******        ")
+print ("                     ")
